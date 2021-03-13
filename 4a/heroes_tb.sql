9008aac4352493823ed37109d27f4342a08b73cd
masterio12 <58644305+masterio12@users.noreply.github.com>
2021-03-13 21:35:24 +0700

    Add files via upload

---
+++ 4a/heroes_tb.sql
@@ -0,0 +1,7 @@
+create table heroes_tb(
+  id varchar (100),
+  name varchar (100),
+  type_id varchar (100),
+  photo varchar (100)
+  );
+  
---
+++ 4a/type_tb.sql
@@ -0,0 +1,8 @@
+create table type_tb(
+  id varchar (100),
+ name  varchar (100)
+  );
+
+
+
+  
